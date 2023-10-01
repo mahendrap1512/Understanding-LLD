3374bf9f841c66c6505c522cd6183a39d2795a50
Mahendra <31130639+mahendrap1512@users.noreply.github.com>
2023-10-01 21:20:40 +0530

    add requirement & fix formatting

--- movie_rental.py
+++ movie_rental.py
@@ -2,9 +2,11 @@ from typing import List, Dict, Tuple
 from enum import Enum
 from datetime import datetime, timedelta
 
+
 class RentStatus(int, Enum):
     AVAILABLE = 0
     RENTED = 1
+
 
 class Movie:
     def __init__(self, _id: int) -> None:
@@ -14,20 +16,31 @@ class Movie:
     def __repr__(self) -> str:
         return f"Movie {self._id}"
 
+
 class Store:
-    def __init__(self, _id: int,) -> None:
+    def __init__(
+        self,
+        _id: int,
+    ) -> None:
         self._id = _id
 
     def __repr__(self) -> str:
         return f"Store {self._id}"
 
+
 class MovieCopy:
-    def __init__(self, _id: int, movie_id: int, store_id: int, price: float, ) -> None:
+    def __init__(
+        self,
+        _id: int,
+        movie_id: int,
+        store_id: int,
+        price: float,
+    ) -> None:
         self.is_available = True
         self._id = _id
         self.movie_id = movie_id
         self.store_id = store_id
-        self.price= price
+        self.price = price
 
     def mark_movie_unavailable(self):
         self.is_available = False
@@ -38,14 +51,18 @@ class MovieCopy:
     def __lt__(self, other):
         if self.price == other.price:
             return self.store._id < other.store._id
-        return self.price < other.price 
-    
+        return self.price < other.price
+
     def __repr__(self) -> str:
         return f"MovieCopy of {self.movie_id} from store {self.store_id} with price {self.price}"
 
 
 class Rental:
-    def __init__(self, _id: Tuple, movie_copy_id: int,) -> None:
+    def __init__(
+        self,
+        _id: Tuple,
+        movie_copy_id: int,
+    ) -> None:
         self._id = _id
         self.movie_copy_id = movie_copy_id
         self.rented_at = datetime.now()
@@ -56,19 +73,16 @@ class Rental:
     def return_movie(self, movie_copy: MovieCopy):
         self.status = RentStatus.AVAILABLE
         from random import randint
+
         # Some random delay to simulate a rented time delay
         self.returned_at = datetime.now() + timedelta(randint(1, 10))
         self.fare = (self.returned_at - self.rented_at).days * movie_copy.price
-
 
     def mark_movie_available(self):
         self.status = RentStatus.AVAILABLE
 
 
-
-
 class MovieRentalSystem:
-
     RESULT_SET_SIZE = 5
 
     def __init__(self) -> None:
@@ -81,14 +95,13 @@ class MovieRentalSystem:
         if key not in self._rentals:
             return False
         return self._rentals[key].status == RentStatus.RENTED
-    
+
     def _find_movie_in_store(self, store_id: int, movie_id: int):
         return next(
             (
                 movie_copy
                 for movie_copy in self._movie_copies.values()
-                if movie_copy.store_id == store_id
-                and movie_copy.movie_id == movie_id
+                if movie_copy.store_id == store_id and movie_copy.movie_id == movie_id
             ),
             None,
         )
@@ -100,8 +113,10 @@ class MovieRentalSystem:
             for movie_copy in self._movie_copies.values()
             if movie_copy.movie_id == movie_id and movie_copy.is_available
         ]
-        return sorted(result, key=lambda movie_copy: movie_copy.price)[:self.RESULT_SET_SIZE]
-    
+        return sorted(result, key=lambda movie_copy: movie_copy.price)[
+            : self.RESULT_SET_SIZE
+        ]
+
     def rent_movie(self, store_id: int, movie_id: int):
         movie_copy = self._find_movie_in_store(store_id, movie_id)
         if not movie_copy:
@@ -122,7 +137,9 @@ class MovieRentalSystem:
         rental = self._rentals[(store_id, movie_id)]
         rental.return_movie(movie_copy)
         movie_copy.mark_movie_available()
-        print(f"movie {movie_id} returned to store {store_id} successfully with rent is {rental.fare}")
+        print(
+            f"movie {movie_id} returned to store {store_id} successfully with rent is {rental.fare}"
+        )
 
     def add_movie_to_store(self, _id: int, movie_id: int, store_id: int, price: float):
         movie_copy = MovieCopy(_id, movie_id, store_id, price)
@@ -130,12 +147,12 @@ class MovieRentalSystem:
 
     def report(self, report_type: str):
         report = Report()
-        return report.get_report_mapping(report_type)(report, self._movie_copies, self._rentals.values(), 5)
+        return report.get_report_mapping(report_type)(
+            report, self._movie_copies, self._rentals.values(), 5
+        )
 
-        
+
 class Report:
-
-
     @classmethod
     def get_report_mapping(cls, report_type: str):
         REPORT_TYPES_TO_REPORT_MAPPING = {
@@ -144,28 +161,42 @@ class Report:
         }
         return REPORT_TYPES_TO_REPORT_MAPPING[report_type]
 
-
-    def _add_movies_from_rentals(self, movie_copies: Dict[int, MovieCopy], rentals: List[Rental]):
+    def _add_movies_from_rentals(
+        self, movie_copies: Dict[int, MovieCopy], rentals: List[Rental]
+    ):
         movies = []
         for rental in rentals:
             movie_copy = movie_copies[rental.movie_copy_id]
             movies.append(movie_copy.movie_id)
         return movies
 
-    def cheapest_movie(self, movie_copies: Dict[int, MovieCopy], rentals: List[Rental], _limit: int,):
-        cheapest_rentals = sorted(rentals, key=lambda x: x.fare)[: _limit]
-        print("cheapest movies", self._add_movies_from_rentals(movie_copies, cheapest_rentals))
+    def cheapest_movie(
+        self,
+        movie_copies: Dict[int, MovieCopy],
+        rentals: List[Rental],
+        _limit: int,
+    ):
+        cheapest_rentals = sorted(rentals, key=lambda x: x.fare)[:_limit]
+        print(
+            "cheapest movies",
+            self._add_movies_from_rentals(movie_copies, cheapest_rentals),
+        )
 
-
-
-    def most_rented_movies(self, movie_copies: Dict[int, MovieCopy], rentals: List[Rental], _limit: int,):
+    def most_rented_movies(
+        self,
+        movie_copies: Dict[int, MovieCopy],
+        rentals: List[Rental],
+        _limit: int,
+    ):
         movies_count = {}
         for rental in rentals:
             movie_id = movie_copies[rental.movie_copy_id].movie_id
             movies_count[movie_id] = movies_count.get(movie_id, 0) + 1
-        most_rented_movies = sorted(movies_count, key=lambda x: movies_count[x], reverse=True)[:_limit]
+        most_rented_movies = sorted(
+            movies_count, key=lambda x: movies_count[x], reverse=True
+        )[:_limit]
         print("most rented movies", most_rented_movies)
-        
+
 
 def driver():
     store1 = Store(1)
@@ -189,29 +220,26 @@ def driver():
     rental_system.add_movie_to_store(8, movie5._id, store3._id, 35)
 
     # Test the search method
-    print(rental_system.search(4))  
-    print(rental_system.search(1)) 
-    print(rental_system.search(2)) 
+    print(rental_system.search(4))
+    print(rental_system.search(1))
+    print(rental_system.search(2))
 
     # Test the rent_movie method
     rental_system.rent_movie(1, 2)
     rental_system.rent_movie(2, 4)
-    rental_system.rent_movie(1, 1)  
-    rental_system.rent_movie(2, 1)  
-
+    rental_system.rent_movie(1, 1)
+    rental_system.rent_movie(2, 1)
 
     # Test the return_movie method
     rental_system.return_movie(2, 4)
     rental_system.return_movie(1, 1)
     rental_system.return_movie(1, 2)
-    rental_system.return_movie(2, 1)  
-
+    rental_system.return_movie(2, 1)
 
     # # Test the report method
-    rental_system.report('cheap_movies')
-    rental_system.report('most_rented')
+    rental_system.report("cheap_movies")
+    rental_system.report("most_rented")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     driver()
